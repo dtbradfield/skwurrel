3d872c0e3b37bbdf7c4ae4513f39b4c4518796c3
DonTheChimera <donaldtbradfield@gmail.com>
2019-04-01 10:13:55 -0500

    now need to do the landing page and I'll be done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingComponent } from './landing/landing.component';
+import { StreamComponent } from './stream/stream.component';
+
+const routes: Routes = [
+  { path: '', component: LandingComponent, pathMatch: 'full' },
+  { path: 'stream', component: StreamComponent },
+  { path: '*', component: LandingComponent },
+  { path: '**', component: LandingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { StreamComponent } from './stream/stream.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    StreamComponent
   ],
   imports: [
     BrowserModule,
